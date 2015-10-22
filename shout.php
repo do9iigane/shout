e1544b173c3a00d4067afdd22f29fd05e596646a
do9iigane <dokumegane@interestic.com>
2015-10-22 13:31:31 +0900

    Changes to be committed: 	modified:   shout.php

--- shout.php
+++ shout.php
@@ -1,12 +1,38 @@
 <?php
-echo GetIpAddr();
+require_once 'vendor/autoload.php';
 
-/*
-  GetIpAddr
-*/
-function GetIpAddr()
-{
-    preg_match_all('/inet ?([^ ]+)/', `/usr/sbin/ifconfig`, $ipaddr);
-//  var_dump($ipaddr);
-    return $ipaddr[1][0];
+//for mac
+$ifconfig  = `/sbin/ifconfig`;
+
+//for pidora
+//$ifconfig  = `/usr/sbin/ifconfig`;
+
+$shout = new Shout();
+$shout->setIfcfg($ifconfig);
+echo $shout->getIpAddr(2);
+
+
+Class Shout {
+
+    public $ifconfig = null;
+
+    public function setIfcfg($ifconfig){
+        $this->ifconfig = $ifconfig;
+    }
+
+    /*
+      GetIpAddr
+    */
+    public function getIpAddr($num=0)
+    {
+        preg_match_all('/^inet\ ?([^ ]+)/', $this->ifconfig, $ipaddr);
+        if(isset($ipaddr[1][$num])){
+            return $ipaddr[1][$num];
+        }else{
+            return '127.0.0.1';
+        }
+
+    }
 }
+
+
